686913266cce13b2c7ee931cc89e5ba893f52890
wefspy <126880105+wefspy@users.noreply.github.com>
2024-05-19 14:51:17 +0500

    update CardComponent

--- src/app/children/dashboard/pages/history/history.component.ts
+++ src/app/children/dashboard/pages/history/history.component.ts
@@ -1,5 +1,6 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {Capacitor, CapacitorGlobal} from "@capacitor/core";
+import {CardModel} from "../../../../data/models/card/card.model";
 
 @Component({
     selector: 'app-history',
@@ -11,4 +12,6 @@ export class HistoryComponent {
 
     protected readonly Capacitor: CapacitorGlobal = Capacitor;
     protected readonly window: Window = window;
+
+    protected readonly myCard: CardModel = new CardModel({balance: 123, dateCreatedTimestamp: 214346436, name: "124", provider: "visa" }, '215')
 }
