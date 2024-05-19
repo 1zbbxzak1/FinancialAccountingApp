e0bf70aa95069a0764263687071a8d0c86fd7516
wefspy <126880105+wefspy@users.noreply.github.com>
2024-05-20 04:07:36 +0500

    add save selected cardId in localStorage

--- src/app/children/dashboard/services/my-cards/card-selection.service.ts
+++ src/app/children/dashboard/services/my-cards/card-selection.service.ts
@@ -4,10 +4,11 @@ import {BehaviorSubject, Observable} from "rxjs";
 @Injectable()
 export class CardSelectionService {
 
-    private selectedCardIdSource: BehaviorSubject<string | null>  = new BehaviorSubject<string | null>(null);
+    private selectedCardIdSource: BehaviorSubject<string | null>  = new BehaviorSubject<string | null>(localStorage.getItem('selectedCardId'));
     public selectedCardId: Observable<string | null> = this.selectedCardIdSource.asObservable();
 
     setSelectedCardId(cardId: string): void {
+        localStorage.setItem('selectedCardId', cardId);
         this.selectedCardIdSource.next(cardId);
     }
 }
